a51adf3bbc421e689fc5e96dd96532f4e9f2104b
Grant Holley <97457524+gh0lree21@users.noreply.github.com>
2023-06-28 11:26:14 -0600

    Initial commit

---
+++ public/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { default as Register } from './pages/Register';
+import { default as Login } from './pages/Login';
+import { default as Chat } from './pages/Chat';
+import { default as SetAvatar } from './pages/SetAvatar';
+
+export default function App()
+{
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/setAvatar" element={<SetAvatar />}/>
+        <Route path="/" element={<Chat />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
